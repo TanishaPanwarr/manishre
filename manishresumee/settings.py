d033f5e007d8d4197540bfdc0e39c53e2c9315de
Tanisha Panwar <tanishapanwar@gmail.com>
2022-11-30 13:40:19 +0530

    first commit

--- manishresumee/settings.py
+++ manishresumee/settings.py
@@ -145,7 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ptanisha922@gmail.com'
-EMAIL_HOST_PASSWORD ='kvqoviodxdxqktci'
+#EMAIL_USE_TLS = True
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'ptanisha922@gmail.com'
+#EMAIL_HOST_PASSWORD ='kvqoviodxdxqktci'
