87bed01d9e788c2382606b5fc47be3ef76129fd2
Tanisha Panwar <tanishapanwar@gmail.com>
2023-01-02 21:31:26 +0530

    commit by manoj

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 # build_files.sh
 pip install -r requirements.txt
-python manage.py migrate
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
 python3.9 manage.py collectstatic
-
+python manage.py migrate --database=auth_db
