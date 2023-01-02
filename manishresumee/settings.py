edba999e9c539f74e444445fa290d53d8274f2d1
Tanisha Panwar <tanishapanwar@gmail.com>
2023-01-02 20:57:19 +0530

    commit by manoj

--- manishresumee/settings.py
+++ manishresumee/settings.py
@@ -145,6 +145,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'staticfiles_build','media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
